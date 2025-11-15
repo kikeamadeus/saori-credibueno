ad2a3da12a3bd0ae3a2a3e52760962fa37308563
Enrique Amadeus Gutierrez Niño <kike.gutierrez90@gmail.com>
2025-11-15 12:58:50 -0600

    Construcción del modulo sucursal edición, eliminción y creación parte 1

--- platform/src/app/main/branches/index.php
+++ platform/src/app/main/branches/index.php
@@ -26,19 +26,32 @@ $branches = getAllBranches();
                                             <path d="M18 6L6.00081 17.9992M17.9992 18L6 6.00085" stroke="#196273" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                         </svg>
                                     </a>
-                                    <dialog id="delete_<?= $branch['id'] ?>" class="modal center-align">
-                                        <h3>¿Estás seguro de eliminar la sucursal de <?= htmlspecialchars($branch['name']) ?>?</h3>
-                                        <a class="btn" href="actions/delete.php?id=<?= $branch['id'] ?>">Aceptar</a>
-                                    </dialog>
                                 </div>
                                 <h2 class="card-title">
                                     <?= htmlspecialchars($branch['name']) ?>
                                     <a data-dialog-open="edit_<?= $branch['id'] ?>" href="#!">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="svg-button" color="currentColor" fill="none">
-                                            <path d="M16.4249 4.60509L17.4149 3.6151C18.2351 2.79497 19.5648 2.79497 20.3849 3.6151C21.205 4.43524 21.205 5.76493 20.3849 6.58507L19.3949 7.57506M16.4249 4.60509L9.76558 11.2644C9.25807 11.772 8.89804 12.4078 8.72397 13.1041L8 16L10.8959 15.276C11.5922 15.102 12.228 14.7419 12.7356 14.2344L19.3949 7.57506M16.4249 4.60509L19.3949 7.57506" stroke="#141B34" stroke-width="2" stroke-linejoin="round" />
-                                            <path d="M18.9999 13.5C18.9999 16.7875 18.9999 18.4312 18.092 19.5376C17.9258 19.7401 17.7401 19.9258 17.5375 20.092C16.4312 21 14.7874 21 11.4999 21H11C7.22876 21 5.34316 21 4.17159 19.8284C3.00003 18.6569 3 16.7712 3 13V12.5C3 9.21252 3 7.56879 3.90794 6.46244C4.07417 6.2599 4.2599 6.07417 4.46244 5.90794C5.56879 5 7.21252 5 10.5 5" stroke="#141B34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M16.4249 4.60509L17.4149 3.6151C18.2351 2.79497 19.5648 2.79497 20.3849 3.6151C21.205 4.43524 21.205 5.76493 20.3849 6.58507L19.3949 7.57506M16.4249 4.60509L9.76558 11.2644C9.25807 11.772 8.89804 12.4078 8.72397 13.1041L8 16L10.8959 15.276C11.5922 15.102 12.228 14.7419 12.7356 14.2344L19.3949 7.57506M16.4249 4.60509L19.3949 7.57506" stroke="#02a58d" stroke-width="2" stroke-linejoin="round" />
+                                            <path d="M18.9999 13.5C18.9999 16.7875 18.9999 18.4312 18.092 19.5376C17.9258 19.7401 17.7401 19.9258 17.5375 20.092C16.4312 21 14.7874 21 11.4999 21H11C7.22876 21 5.34316 21 4.17159 19.8284C3.00003 18.6569 3 16.7712 3 13V12.5C3 9.21252 3 7.56879 3.90794 6.46244C4.07417 6.2599 4.2599 6.07417 4.46244 5.90794C5.56879 5 7.21252 5 10.5 5" stroke="#02a58d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                         </svg>
                                     </a>
+                                    <dialog id="edit_<?= $branch['id'] ?>" class="modal">
+                                        <div class="dialog-action">
+                                            <a data-dialog-close href="#!">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="currentColor" fill="none">
+                                                    <path d="M18 6L6.00081 17.9992M17.9992 18L6 6.00085" stroke="#196273" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                                </svg>
+                                            </a>
+                                        </div>
+                                        <div class="dialog-content center-align">
+                                            <h2>Editar Sucursal</h2>
+                                            <form action="#" method="POST">
+                                                <div class="input-field">
+                                                    <input placeholder="Nombre Sucursal" name="branch_name" value="<?= htmlspecialchars($branch['name']) ?>" type="text">
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </dialog>
                                 </h2>
                                 <div class="divider"></div>
                                 <p class="card-texts"><?= htmlspecialchars($branch['address']) ?></p>
@@ -52,6 +65,20 @@ $branches = getAllBranches();
                                 </a>
                             </div>
                         </div>
+
+                        <dialog id="delete_<?= $branch['id'] ?>" class="modal center-align">
+                            <div class="dialog-action">
+                                <a data-dialog-close href="#!">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="currentColor" fill="none">
+                                        <path d="M18 6L6.00081 17.9992M17.9992 18L6 6.00085" stroke="#196273" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </a>
+                            </div>
+                            <div class="dialog-content">
+                                <h3>¿Estás seguro de eliminar la sucursal de <?= htmlspecialchars($branch['name']) ?>?</h3>
+                                <a class="btn" href="actions/delete.php?id=<?= $branch['id'] ?>">Aceptar</a>
+                            </div>
+                        </dialog>
                     <?php endforeach; ?>
                 </div>
             <?php endif; ?>
